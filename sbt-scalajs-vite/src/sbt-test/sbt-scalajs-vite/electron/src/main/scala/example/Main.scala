227dbd3fea98ea2f400b18287eedb54b5806140d
Domantas Petrauskas <dom.petrauskas@gmail.com>
2023-01-17 22:33:28 +0200

    Implement electron quick start

--- sbt-scalajs-vite/src/sbt-test/sbt-scalajs-vite/electron/src/main/scala/example/Main.scala
+++ sbt-scalajs-vite/src/sbt-test/sbt-scalajs-vite/electron/src/main/scala/example/Main.scala
@@ -12,12 +12,7 @@ import scala.scalajs.js
 import scala.scalajs.js.|
 
 object Main extends App {
-  println("Main works")
-
-  val url = process.env.VITE_DEV_SERVER_URL.asInstanceOf[String]
-
-  println(s"__dirname ${__dirname}")
-
+  // Create the browser window.
   def createWindow(): Unit = {
     val mainWindow = new BrowserWindow(new BrowserWindowConfig {
       override val height = 600
@@ -27,18 +22,43 @@ object Main extends App {
       }
     })
 
+    // and load the index.html of the app.
+    val url = process.env.VITE_DEV_SERVER_URL.asInstanceOf[String]
     if (url != null) {
       mainWindow.loadURL(url)
     } else {
       mainWindow.loadFile(join(__dirname, "dist", "index.html"))
     }
+
+    // Open the DevTools.
+    // mainWindow.webContents.openDevTools()
   }
 
+  // This method will be called when Electron has finished
+  // initialization and is ready to create browser windows.
+  // Some APIs can only be used after this event occurs.
   app
     .whenReady()
     .`then`((_ => {
       createWindow()
 
-      ()
+      // On macOS it's common to re-create a window in the app when the
+      // dock icon is clicked and there are no other windows open.
+      app.on(
+        "activate",
+        () => {
+          if (BrowserWindow.getAllWindows().length == 0) createWindow()
+        }
+      )
     }): js.Function1[Unit, Unit | js.Thenable[Unit]])
+
+  // Quit when all windows are closed, except on macOS. There, it's common
+  // for applications and their menu bar to stay active until the user quits
+  // explicitly with Cmd + Q.
+  app.on(
+    "window-all-close",
+    () => {
+      if (process.platform != "darwin") app.quit()
+    }
+  )
 }
--- sbt-scalajs-vite/src/sbt-test/sbt-scalajs-vite/electron/src/main/scala/example/Preload.scala
+++ sbt-scalajs-vite/src/sbt-test/sbt-scalajs-vite/electron/src/main/scala/example/Preload.scala
@@ -1,5 +1,34 @@
 package example
 
+import example.facade.node.NodeGlobals.process
+import org.scalajs.dom.Event
+import org.scalajs.dom.document
+import org.scalajs.dom.window
+
+import scala.scalajs.js
+
+/** The preload script runs before. It has access to web APIs as well as
+  * Electron's renderer process modules and some polyfilled Node.js functions.
+  *
+  * https://www.electronjs.org/docs/latest/tutorial/sandbox
+  */
 object Preload extends App {
   println("Preload works")
+  window.addEventListener(
+    "DOMContentLoaded",
+    (_ => {
+      val replaceText = (selector: String, text: String) => {
+        val element = document.getElementById(selector)
+        if (element != null) element.innerText = text
+      }
+
+      js.Array("chrome", "node", "electron")
+        .foreach(`type` =>
+          replaceText(
+            s"${`type`}-version",
+            process.versions.get(`type`).orNull
+          )
+        )
+    }): js.Function1[Event, Unit]
+  )
 }
