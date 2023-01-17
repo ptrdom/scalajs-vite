825b211f517f7a11edeb2766ab555c3b5b697e5b
Domantas Petrauskas <dom.petrauskas@gmail.com>
2023-01-17 22:33:28 +0200

    Implement electron quick start

--- sbt-scalajs-vite/src/sbt-test/sbt-scalajs-vite/electron/src/main/scala/example/Renderer.scala
+++ sbt-scalajs-vite/src/sbt-test/sbt-scalajs-vite/electron/src/main/scala/example/Renderer.scala
@@ -1,5 +1,9 @@
 package example
 
-object Renderer extends App {
-  println("Renderer works")
-}
+/** This file is loaded via the <script> tag in the index.html file and will be
+  * executed in the renderer process for that window. No Node.js APIs are
+  * available in this process because `nodeIntegration` is turned off and
+  * `contextIsolation` is turned on. Use the contextBridge API in `preload.js`
+  * to expose Node.js functionality from the main process.
+  */
+object Renderer extends App {}
